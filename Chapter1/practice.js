a96a66e60ed986aa8f702bbe9e5be1e31b3a614b
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-04 17:15:24 +0530

    Add exercise from Chapter1

---
+++ Chapter1/practice.js
@@ -0,0 +1,31 @@
+Infinity === Infinity
+-Infinity === -Infinity
+NaN === NaN
+Infinity === -Infinity
+"Patch my boat with chewing gum"
+'Monkey\'s wave goodbye'
+"A newline character is written like \"\\n\"."
+typeof 4.5
+typeof 'x'
+typeof 4
+'Aadvark' < 'Aadvarg'
+'Aadvark' < 'Aadvarl'
+true && false
+true && true
+!true
+!false
+1 + 1 === 2 && 10 * 10 > 50
+true ? 1 : 2
+false ? 1 : 2
+8 * null
+'5' - 1
+"5" + 1
+"five" * 2
+false === 0
+null == undefined // -> true
+null == 0 // -> false
+undefined == 0
+undefined == null
+null || 'user'
+'karl' || 'user'
+false && 10
