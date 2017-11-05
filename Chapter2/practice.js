f9faf91ac13c9263e116b31a817968d2d9f51c76
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-06 00:47:09 +0530

    Add exercise from Chapter2

---
+++ Chapter2/practice.js
@@ -0,0 +1,13 @@
+1;
+!false;
+/* A statement stand on it's own and amounts to something only if it affects the world. It could display something on the screen - that counts as changing the world - or it could change the internal state of the machine in an way that will affect the statements that come after it. These changes are called side effects. */
+// Learn subtleties involved in leaving out semicolons
+var luigisDebt = 140;
+luigisDebt = luigisDebt - 32;
+console.log(luigisDebt);
+var one = 1, two = 2
+console.log(one + two);
+var theNumber = Number(prompt("Pick a number", ""));
+if (!isNaN(theNumber)) alert("Your number is the square root of "+ theNumber * theNumber)
+else
+  alert("Hey. Why didn't you give me a number?")
