7221f1b2a1b99deea50d45fb0beee3793d9cbbe8
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-11 20:15:43 +0530

    Add checkType function

--- Chapter1/practice.js
+++ Chapter1/practice.js
@@ -8,6 +8,33 @@ Infinity === -Infinity
 typeof 4.5
 typeof 'x'
 typeof 4
+const checkType = (obj) => ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+console.log('checkType ------------------------------------------>')
+console.log(checkType({a: 4}))
+console.log(checkType([1, 2, 3]))
+console.log(checkType(new ReferenceError))
+console.log(checkType(new Date))
+console.log(checkType(/a-z/))
+console.log(checkType(Math))
+console.log(checkType(JSON))
+console.log(checkType(new Number(4)))
+console.log(checkType(new String('abc')))
+console.log(checkType(new Boolean('true')))
+console.log(checkType(null))
+console.log(checkType(true))
+console.log(checkType(false))
+console.log('Typeof ------------------------------------------>')
+console.log(typeof {a: 4}); //"object"
+console.log(typeof [1, 2, 3]); //"object"
+(function() {console.log(typeof arguments)})(); //object
+console.log(typeof new ReferenceError); //"object"
+console.log(typeof new Date); //"object"
+console.log(typeof /a-z/); //"object"
+console.log(typeof Math); //"object"
+console.log(typeof JSON); //"object"
+console.log(typeof new Number(4)); //"object"
+console.log(typeof new String("abc")); //"object"
+console.log(typeof new Boolean(true)); //"object"
 'Aadvark' < 'Aadvarg'
 'Aadvark' < 'Aadvarl'
 true && false
