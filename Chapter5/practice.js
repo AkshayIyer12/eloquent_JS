8650d7776bc66d747bc1233e6e5afd88d051aaea
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-27 00:13:33 +0530

    Add sharedDNA and reduceAncestors

--- Chapter5/practice.js
+++ Chapter5/practice.js
@@ -156,3 +156,32 @@ function female (p) {
 console.log(average(ancestry.filter(male).map(age)))
 
 console.log(average(ancestry.filter(female).map(age)))
+
+let byName = {}
+ancestry.forEach(function (person) {
+  byName[person.name] = person
+})
+
+console.log(byName['Philibert Haverbeke'])
+
+function reduceAncestors (person, f, defaultValue) {
+  function valueFor (person) {
+    if (person === null || person === undefined) {
+      return defaultValue
+    } else {
+      return f(person, valueFor(byName[person.mother]), valueFor(byName[person.father]))
+    }
+  }
+  return valueFor(person)
+}
+
+function sharedDNA (person, fromMother, fromFather) {
+  if (person.name === 'Pauwels van Haverbeke') {
+    return 1
+  } else {
+    return (fromMother + fromFather) / 2
+  }
+}
+
+let ph = byName['Philibert Haverbeke']
+console.log(reduceAncestors(ph, sharedDNA, 0) / 4)
